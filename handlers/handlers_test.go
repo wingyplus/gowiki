9e65183a11aa33d43a08d0395eb806c54699351e
Thanabodee <thanabodee@osdev.co.th>
2014-02-03 01:00:20 +0700

    move to suite

---
+++ handlers/handlers_test.go
@@ -0,0 +1,12 @@
+package handlers_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func TestHandlers(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Handler Spec")
+}
